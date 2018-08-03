b2b4861ba6ac3ee8500afe35831a2c273a7b636c
BenChoi <wchoi19@illinois.edu>
2018-08-03 10:49:04 +0900

    코멘트달기

--- Scaffold_Element.java
+++ Scaffold_Element.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 
 import org.jdom2.Element;
 //commit test
+/*
+    이 클래스는 xml 스캐폴딩 할 시에 xml element들의 이름을 넣어주는 기능이다
+    "scaffold", "func_name", "ret_type", "args", "body" 등이 있다.
+ */
 public class Scaffold_Element {
-    Scaffold_Element(){
-
-    }
+    Scaffold_Element(){ }
     public Element SC = new Element("scaffold");
     public Element func_name = new Element("func_name");
     public Element ret_type = new Element("ret_type");
@@ -19,8 +21,5 @@ public class Scaffold_Element {
 
     public ArrayList<Element> Argument_list = new ArrayList<Element>();
     public ArrayList<Element> Func_arg_list = new ArrayList<Element>();
-
-
-
 
 }
